48ac0da4e36a0732fc3976d87718d30d6fb3b2ef
prrism <yumatezu@gmail.com>
2024-07-01 09:34:19 +0900

    HoT: initial revision

---
+++ HeadsORTails.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import random
+
+class Coin:
+    sides = ['Heads', 'Tails']
+
+    def flip():
+        return random.choice(Coin.sides)
+
+if __name__ == '__main__':
+    num_rounds = 3
+    num_heads = 0
+    num_tails = 0
+
+    for i in range(num_rounds):
+        flip = Coin.flip()
+        if flip == 'Heads':
+            num_heads += 1
+        else:
+            num_tails += 1
+        print("Round {}: {}".format(i+1, flip))
+    print("Heads: {}, Tails: {}".format(num_heads, num_tails))
